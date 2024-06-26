3cd9423a4326b9d9f0235aa28fedb6bb3c1f9498
niilliiz <niloo.vatan.nv@@gmail.com>
2024-06-26 08:13:57 +0330

    ...

--- src/Algorithms/searches/binary-search/index.ts
+++ src/Algorithms/searches/binary-search/index.ts
@@ -1,11 +1,11 @@
-import { getRandomArray } from "../../../utils/array-random";
+import { getSortedRandomArray } from "../../../utils/array-random";
 
 function BinarySearch(arr: number[], value: number): number {
   let low = 0;
   let high = arr.length - 1;
 
   while (low <= high) {
-    let mid = Math.floor((high + low) / 2);
+    const mid = Math.floor((high + low) / 2);
 
     if (arr[mid] === value) {
       return mid;
@@ -21,4 +21,4 @@ function BinarySearch(arr: number[], value: number): number {
   return -1;
 }
 
-BinarySearch(getRandomArray(), 45);
+BinarySearch(getSortedRandomArray(), -8);
--- src/utils/array-random.ts
+++ src/utils/array-random.ts
@@ -9,4 +9,15 @@ function getRandomArray(
   );
 }
 
-export { getRandomArray };
+function getSortedRandomArray(
+  length: number = 100,
+  min: number = -100,
+  max: number = 100,
+): number[] {
+  return Array.from(
+    Array(length),
+    () => Math.floor(Math.random() * (max - min + 1)) + min,
+  ).sort((a,b)=>a-b);
+}
+
+export { getRandomArray,getSortedRandomArray };
