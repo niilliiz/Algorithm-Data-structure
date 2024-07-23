faa43ba13d38408c3f833a9d7ab865203e004751
niilliiz <n.vatanara@mydoki.com>
2024-07-23 08:45:23 +0330

    stack initialized

---
+++ src/DS/stack/stack.ts
@@ -0,0 +1,7 @@
+// tsx src/DS/linked-list/linked-list.ts command
+
+import { IStack } from "../../interfaces/interface-stack";
+
+class Stack<T> implements IStack<T> {}
+
+const ll = new Stack();
---
+++ src/interfaces/interface-stack.ts
@@ -0,0 +1,1 @@
+export interface IStack<T> {}
