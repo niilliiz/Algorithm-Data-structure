2527285589b714f3924c1ba1b8cf7c41fc607dd3
niilliiz <n.vatanara@mydoki.com>
2024-08-03 11:02:50 +0330

    stack initialize interface

--- src/DS/stack/stack.ts
+++ src/DS/stack/stack.ts
@@ -2,6 +2,39 @@
 
 import { IStack } from "../../interfaces/interface-stack";
 
-class Stack<T> implements IStack<T> {}
+class Stack<T> implements IStack<T> {
+  readonly size: number;
+  private top: number;
+  private array: Array<T>;
+
+  constructor(size: number) {
+    this.array = new Array<T>(size);
+    this.size = size;
+    this.top = -1;
+  }
+
+  get count(): number {
+    return 0;
+  }
+
+  isEmpty(): boolean {
+    return true;
+  }
+  isFull(): boolean {
+    return true;
+  }
+
+  peek(): T | undefined {
+    return undefined;
+  }
+  pop(): T | undefined {
+    return undefined;
+  }
+  push(data: T): Stack<T> {}
+  empty(): void {}
+  contains(item: T): boolean {
+    return false;
+  }
+}
 
 const ll = new Stack();
--- src/interfaces/interface-stack.ts
+++ src/interfaces/interface-stack.ts
@@ -1,1 +1,16 @@
-export interface IStack<T> {}
+export interface IStack<T> {
+  readonly size: number;
+
+  get count(): number;
+
+  isEmpty(): boolean;
+  isFull(): boolean;
+
+  peek(): T | undefined;
+  push(data: T): IStack<T>;
+  pop(): T | undefined;
+
+  empty(): void;
+
+  contains(item: T): boolean;
+}
