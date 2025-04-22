0cbd79c3fa6a1a4ecfb49ab74e1ed3ab7c6fc6f4
niilliiz <niloo.vatan.nv@gmail.com>
2025-04-22 08:03:52 +0330

    feat: new coming

--- src/Algorithms/searches/binary-search/binary-search.test.ts
+++
@@ -1,9 +0,0 @@
-import { BinarySearch } from './index';
-
-test('Binary Search: Not Found', () => {
-  expect(BinarySearch([1,2,3],4)).toBe(-1);
-});
-
-test('Binary Search: Found', () => {
-  expect(BinarySearch([1,2,3], 3)).toBe(2);
-});
