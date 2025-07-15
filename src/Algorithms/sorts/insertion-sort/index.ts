3cd1519114fad3ab9cd231041aff55827a4319a6
niilliiz <niloo.vatan.nv@gmail.com>
2025-07-15 09:20:08 +0330

    feat: snippet has bug

--- src/Algorithms/sorts/insertion-sort/index.ts
+++ src/Algorithms/sorts/insertion-sort/index.ts
@@ -1,1 +1,19 @@
-export function InsertionSort(arr: number[]): number[] {}
+export function InsertionSort(arr: number[]): number[] {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      for (let j = i - 1; j >= 0; j--) {
+        if (arr[i] < arr[i - 1]) {
+          [arr[i], arr[j]] = [arr[j], arr[i]];
+        } else {
+          break;
+        }
+      }
+    }
+  }
+
+  return arr;
+}
