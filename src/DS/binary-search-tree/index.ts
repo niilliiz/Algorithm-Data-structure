3d9c77938cd351e97255187f37684500add998a5
niilliiz <niloo.vatan.nv@gmail.com>
2025-09-01 08:53:57 +0330

    refactor: update binary search tree insertion logic and adjust interface

--- src/DS/binary-search-tree/index.ts
+++ src/DS/binary-search-tree/index.ts
@@ -3,32 +3,58 @@ import { IBinaryTreeNode } from "../../interfaces/IBinary-Tree-Node";
 
 export class BinarySearchTree<T> implements IBinarySearchTree<T> {
   private root: IBinaryTreeNode<T> | null;
+
   constructor() {
     this.root = null;
   }
 
-  insert(node: IBinaryTreeNode<T>, value: T): T {
-    if (this.isEmpty()) {
-      this.root = { value, left: null, right: null };
-      return;
-    }
-
-    if (node === null) {
-      return value;
-    }
-
-    if (node.value < value) {
-      this.insert(node.left, value);
-    } else {
-      this.insert(node.right, value);
-    }
+  insert(value: T): void {
+    this.root = this.insertNode(this.root, value);
   }
+
+  private insertNode(
+    node: IBinaryTreeNode<T> | null,
+    value: T,
+  ): IBinaryTreeNode<T> {
+    if (node === null) {
+      return { value, left: null, right: null };
+    }
+
+    if (value < node.value) {
+      node.left = this.insertNode(node.left, value);
+    } else {
+      node.right = this.insertNode(node.right, value);
+    }
+
+    return node;
+  }
+
   delete(value: T): T | undefined {
     if (this.isEmpty()) return undefined;
+    // TODO: Implement deletion logic
   }
+
   search(value: T): T | undefined {
     if (this.isEmpty()) return undefined;
+    return this.searchNode(this.root, value);
   }
+
+  private searchNode(node: IBinaryTreeNode<T> | null, value: T): T | undefined {
+    if (node === null) {
+      return undefined;
+    }
+
+    if (value === node.value) {
+      return node.value;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else {
+      return this.searchNode(node.right, value);
+    }
+  }
+
   isEmpty(): boolean {
     return this.root === null;
   }
--- src/interfaces/IBinary-Search-Tree.ts
+++ src/interfaces/IBinary-Search-Tree.ts
@@ -1,7 +1,7 @@
 import { IBinaryTreeNode } from "./IBinary-Tree-Node";
 
 export interface IBinarySearchTree<T> {
-  insert(node: IBinaryTreeNode<T>, value: T): void;
+  insert(value: T): void;
   delete(value: T): T | undefined;
   search(value: T): T | undefined;
   isEmpty(): boolean;
