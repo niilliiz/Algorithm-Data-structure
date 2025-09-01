bde26a91f12559b00b485974c1e085680875d7e7
niilliiz <niloo.vatan.nv@gmail.com>
2025-09-01 08:59:08 +0330

    refactor: remove compiled j files

--- src/DS/queue-priority/index.js
+++
@@ -1,2 +0,0 @@
-// todo: first learn or write  heap or at least one one of the sorting algorithms
-//  and then write this DS
--- src/utils/array-random.js
+++
@@ -1,7 +0,0 @@
-function getRandomArray(length = 100, min = -100, max = 100) {
-    return Array.from(Array(length), () => Math.floor(Math.random() * (max - min + 1)) + min);
-}
-function getSortedRandomArray(length = 100, min = -100, max = 100) {
-    return Array.from(Array(length), () => Math.floor(Math.random() * (max - min + 1)) + min).sort((a, b) => a - b);
-}
-export { getRandomArray, getSortedRandomArray };
