358de52ea2e9a873d5873063c0cac6369817f576
niilliiz <niloo.vatan.nv@gmail.com>
2025-10-07 07:18:08 +0330

    fix: clarify parent property in IAVLNode interface as root indicator

--- src/interfaces/IAVL-Tree.ts
+++ src/interfaces/IAVL-Tree.ts
@@ -3,7 +3,7 @@ export type NodeObj<T> = {
   height: number;
   left: number; // -1 means none
   right: number; // -1 means none
-  parent: number;
+  parent: number; // -1 means root
 };
 
 export interface IAVLTree<T> {
