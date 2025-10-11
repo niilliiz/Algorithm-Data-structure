1273d6d30e70ed13d7818b1c1a48b8f663456ed5
vatanara.niloufar <niloo.vatan.nv@gmail.com>
2025-10-11 18:17:28 +0330

    feat: add multiple implementations for two-sum problem

---
+++ src/NeetCode/two-sum.ts
@@ -0,0 +1,53 @@
+// number of pairs => n(n-1)/2
+// time complexity: O(n^2)
+// space: O(1)
+function twoSum_bruteForce(
+  arr: number[],
+  target: number,
+): [i: number, j: number] | "not found" {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+
+  return "not found";
+}
+
+// time and space: O(n)
+function twoSum_obj(
+  nums: number[],
+  target: number,
+): [number, number] | "not found" {
+  const map: Record<number, number> = {}; // stores number → index
+
+  for (let i = 0; i < nums.length; i++) {
+    const needed = target - nums[i];
+
+    if (map[needed] !== undefined) {
+      return [map[needed], i];
+    }
+
+    map[nums[i]] = i;
+  }
+
+  return "not found";
+}
+
+function twoSum_map(arr: number[], target: number): [number, number] | null {
+  const mapped = new Map<number, number>();
+
+  for (let i = 0; i < arr.length; i++) {
+    const minus = target - arr[i];
+
+    if (mapped.has(minus)) {
+      return [mapped.get(minus)!, i];
+    }
+
+    mapped.set(arr[i], i);
+  }
+
+  return null;
+}
