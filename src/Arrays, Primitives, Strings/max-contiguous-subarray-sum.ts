de41a6b747981818913368bbeab9fabc4be34bd4
niilliiz <niloo.vatan.nv@gmail.com>
2026-05-05 07:07:59 +0330

    docs: add example for max contiguous subarray sum function

--- src/Arrays, Primitives, Strings/max-contiguous-subarray-sum.ts
+++ src/Arrays, Primitives, Strings/max-contiguous-subarray-sum.ts
@@ -1,5 +1,10 @@
 // O(n)
 
+/**
+ * arr: [−2, 1, −3, 4, −1, 2, 1, −5, 4]
+ * solution: { subArray: [ 4, -1, 2, 1 ], maxSum: 6 }
+ */
+
 export default function MaxContiguousSubArraySum(arr: number[]) {
   let currentSum = 0;
   let maxSum = -Infinity;
